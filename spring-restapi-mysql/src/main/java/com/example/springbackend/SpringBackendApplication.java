87096c18e17dc1a0f992b52aa649715a2ed72809
GnanaJeyam <gnanajeyam95@gmail.com>
2019-04-15 17:41:02 +0530

    Added @EnableDiscoveryClient to connect with Spring Eureka Server.

--- spring-restapi-mysql/src/main/java/com/example/springbackend/SpringBackendApplication.java
+++ spring-restapi-mysql/src/main/java/com/example/springbackend/SpringBackendApplication.java
@@ -2,13 +2,14 @@ package com.example.springbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendApplication.class, args);
 			
 	}
-
 }
