f9c0a85d1eaada103c749d13343e2a2add83fc8e
GnanaJeyam <gnanajeyam95@gmail.com>
2019-04-16 17:43:37 +0530

    Added CROSS Header to suppport all origin. Overrides the default request method TYPES.

--- spring-restapi-mysql/src/main/java/com/example/springbackend/security/SecurityConfig.java
+++ spring-restapi-mysql/src/main/java/com/example/springbackend/security/SecurityConfig.java
@@ -1,25 +1,48 @@
 package com.example.springbackend.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		http.authorizeRequests()
 			.antMatchers("/greetmessage").permitAll()
+			.antMatchers("/v2/api-docs").permitAll()
 	        .antMatchers(HttpMethod.POST, "/refresh").permitAll()
 			.antMatchers("/health").permitAll()
 			.antMatchers("/students/{id}").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
+			// Enabling CROSS request for all origins and overriding default request TYPES
+			.cors().configurationSource(request -> 
+				{
+					CorsConfiguration cors = new CorsConfiguration();
+						cors.setAllowedMethods(
+							Arrays.asList(
+								HttpMethod.DELETE.name(),
+								HttpMethod.GET.name(), 
+								HttpMethod.HEAD.name(), 
+								HttpMethod.POST.name(),
+								HttpMethod.PUT.name()
+							));
+						cors.applyPermitDefaultValues();
+					
+					return cors;
+					
+				})
+			.and()
 			.csrf().disable()
 			.httpBasic();
 		
 	}
+
 }
